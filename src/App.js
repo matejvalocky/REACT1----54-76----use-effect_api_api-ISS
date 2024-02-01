ca6bd436477210c9f4f0a2132649d00250247861
Matej Valocký <matej.valocky@gmail.com>
2024-02-01 22:23:45 +0100

    55. React - Ukázka useEffectu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import { useState, useEffect } from "react"
 
 const App = () => {
+
+
+  useEffect(() => {
+    console.log("ja som useEffect");
+  })
+
+  // useEffect sa spúšťa až na konci
+
+
+
+
+
   return (
-    <div>App</div>
+    <div>
+      <h1>začíname</h1>
+      {console.log("ja som return")}
+    </div>
   )
 }
 
