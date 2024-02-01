a1ae9c7ed03df37a5b4c6eae5b2d4c0d314907fe
Matej Valocký <matej.valocky@gmail.com>
2024-02-01 22:39:33 +0100

    58. React - Příklad využití useEffectu

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import { useState, useEffect } from "react"
 
 const App = () => {
 
-  console.log("text úplne hore")
 
   const [value, setValue] = useState(0)
 
-  
+
 
 
   useEffect(() => {
     console.log("ja som useEffect");
+    const button = document.querySelector(".btn")
+    button.textContent = `Klik číslo ${value}`
   })
 
   // useEffect sa spúšťa až na konci
 
 
 
-console.log("text pred return")
+
 
   return (
     <div>
       <h1>začíname</h1>
       <p>{value}</p>
       {console.log("ja som return")}
-      <button onClick={ () => setValue (value + 1)}>Klikni</button>
+      <button className="btn" onClick={() => setValue(value + 1)}>Klikni</button>
     </div>
   )
 }
