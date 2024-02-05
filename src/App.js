6c193031a9f66d0fe253af08281410880f47706b
Matej Valocký <matej.valocky@gmail.com>
2024-02-05 22:46:56 +0100

    62. React - předěláme App (resize)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
 import { useState, useEffect } from "react"
 
 const App = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
 
+  const sizeControl = () => {
+    setWindowSize(window.innerWidth)
+  }
 
-  const [value, setValue] = useState(0)
-  const [test, setTest] = useState(0)
-
-
-  // useEffect nemôžeme obaľovať do podmienky
-  // useEffect beží len raz ak na konci pridáme []
-
-  useEffect(() => {
-    const button = document.querySelector(".btn")
-    if (value >= 1) {
-      button.textContent = `Klik číslo ${value}`
-    } else {
-      button.textContent = "klikni"
-    }
-
-    console.log("Prvý useEffect - Klikni")
-
-
-
-  }, [value]) // spustí sa len vtedy keď sa zmení value
-
-  // useEffect sa spúšťa až na konci
-
-  useEffect(() => {
-    document.title = `Nový titulok ${test}`
-    console.log("Druhý useEffect - titulok")
-  },[test]) // spustí sa len vtedy keď sa zmení test
-
-
-
-
+  useEffect ( () => {
+    window.addEventListener("resize", sizeControl)
+  })
 
   return (
     <div>
-      <h1>začíname</h1>
-      <p>{value}</p>
-      
-      <button className="btn" onClick={() => setValue(value + 1)}>Klikni</button>
-
-      <button className="btn-test" onClick={() => setTest(test + 1)}>Titulok</button>
+      <h1>Šírka okna</h1>
+      <h2>{windowSize}</h2>
     </div>
   )
 }
