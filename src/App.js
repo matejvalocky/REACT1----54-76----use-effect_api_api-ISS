34a9a874f3646dcf7918272a75f5867172801a8d
Matej Valocký <matej.valocky@gmail.com>
2024-02-06 22:04:44 +0100

    71. React - Vypsání, které funguje (useState, useEffect)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useState, useEffect } from "react"
+
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
-  let latitude = ""
-  let longitude = ""
+
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
 
   const getCoordinates = async () => {
     const response = await fetch(url) // fetch daj mi tie dáta // await - počkaj
     const data = await response.json() // prekonvertovanie dat
-    console.log(data["iss_position"]["latitude"]) // zemepisna šírka
-    console.log(data["iss_position"]["longitude"]) // zemepisna dĺžka
-    latitude = data["iss_position"]["latitude"]
-    longitude = data["iss_position"]["longitude"]
+
+
+    setLatitude(data["iss_position"]["latitude"])
+    setLongitude(data["iss_position"]["longitude"])
+
+
   }
 
-  getCoordinates()
+  useEffect(() => {
+    getCoordinates()
+  }, [])
+
+
 
 
   return (
