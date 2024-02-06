350deb5b14ad6cfdeb942802d1a4009d5a78e347
Matej Valocký <matej.valocky@gmail.com>
2024-02-06 21:55:15 +0100

    70. React - Vypsání zeměpisné šířky a délky ISS do stránky nefunguje

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
+  let latitude = ""
+  let longitude = ""
 
-  const getCoordinates = async() => {
+  const getCoordinates = async () => {
     const response = await fetch(url) // fetch daj mi tie dáta // await - počkaj
     const data = await response.json() // prekonvertovanie dat
     console.log(data["iss_position"]["latitude"]) // zemepisna šírka
     console.log(data["iss_position"]["longitude"]) // zemepisna dĺžka
+    latitude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
   }
 
   getCoordinates()
 
 
   return (
-    <h1>API</h1>
+    <div>
+      <h1>API</h1>
+      <h2>Zemepisná šírka</h2>
+      <p>{latitude}</p>
+      <h2>Zemepísná dĺžka</h2>
+      <p>{longitude}</p>
+    </div>
+
   )
 }
 
