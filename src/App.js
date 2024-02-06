1d47ebba5f5ce38e7a31425f5724ce8a8aff6836
Matej Valocký <matej.valocky@gmail.com>
2024-02-06 22:40:00 +0100

    74. React - Jak lépe pracovat s API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 // Multiple returns
 
+const url = "http://api.open-notify.org/iss-now.json"
+
+import { useState, useEffect } from "react"
+
 const App = () => {
 
-  const loading = false
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data["iss_position"])
+      .then((position) => {
+          console.log(position["latitude"])
+          console.log(position["longitude"])
+      })
+  }, [])
+
+
 
   if (loading) {
     return <h2>Načítanie stránky</h2>
